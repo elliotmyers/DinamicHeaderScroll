b795e10034d4fd612cb98c60db18ec94dfbe67ab
elliotmyers <elliotmyers47@gmail.com>
2023-04-03 14:56:51 +0300

    Initial Commit

---
+++ HeaderScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeaderScroll
+//
+//  Created by Raiden Yamato on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
